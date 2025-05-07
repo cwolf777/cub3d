3be60023afd9f052152a8a087c54c8968e5da46b
Philipp Hofmann <phofmann200@gmail.com>
2025-05-07 09:41:03 +0200

    remove debugging printf in validate_player

--- src/validation/validation.c
+++ src/validation/validation.c
@@ -35,13 +35,11 @@ bool	validate_player(t_map map)
 
 	player_count = 0;
 	y = 0;
-	print_grid(map.grid);
 	while (y < map.height)
 	{
 		x = 0;
 		while (x < map.width)
 		{
-			printf("%c", map.grid[y][x]);
 			if (ft_strchr("NWES", map.grid[y][x]))
 			{
 				// printf("c: %c\n", map.grid[y][x]);
@@ -49,7 +47,6 @@ bool	validate_player(t_map map)
 			}
 			x++;
 		}
-		printf("\n");
 		y++;
 	}
 	if (player_count == 1)
